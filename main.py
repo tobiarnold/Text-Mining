83acb79cefb5c99adbb1f31311c5c63a62260cfe
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2022-04-26 12:13:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -61,8 +61,7 @@ def main():
     st.title("📊 Countplots der Sentiments")
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9, 3))
     ax=sns.countplot(x ="sentiment_textblob", data = df_countplot).set(title="Anzahl Sentiments textblob")
-    for p in ax.patches:
-        ax.annotate('{:.2f}%'.format(p.get_height()), (p.get_x()+0.15, p.get_height()+1))
+    ax.bar_label(ax.containers[0])
     st.pyplot(fig)
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9, 3))
     ax=sns.countplot(x ="sentiment_nltk", data = df_countplot).set(title="Anzahl Sentiments nltk")
